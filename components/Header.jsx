75875473fb9720f414c8daf77ec6da8ccd40736c
Deen <gingerg238@gmail.com>
2022-12-24 19:29:02 +0200

    Create Pizza Shop

---
+++ components/Header.jsx
@@ -0,0 +1,30 @@
+import css from '../styles/Header.module.css'
+import Image from 'next/image'
+import Logo from '../assets/Logo.png'
+import {UilShoppingBag} from '@iconscout/react-unicons'
+export default function Header () {
+  return (
+    <div className={css.header}>
+        {/* logo side */}
+        <div className={css.log}>
+            <Image src={Logo} alt="" width={50} height={50}/>
+            <span>Fudo</span>
+        </div>
+
+        {/* menu side */}
+        <ul className={css.menu}>
+            <li>Home</li>
+            <li>Menu</li>
+            <li>Contact</li>
+        </ul>
+
+        {/* right side */}
+        <div className={css.rightSide}>
+            <div className={css.cart}>
+                <UilShoppingBag />
+            </div>
+        </div>
+    </div>
+  )
+}
+
