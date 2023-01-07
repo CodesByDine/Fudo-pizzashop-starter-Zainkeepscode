ab3097ff4fff2e3832fd742e8e1e4f5a314dc999
Deen <gingerg238@gmail.com>
2023-01-07 17:30:28 +0200

    Sanity Installed

---
+++ sanity/schemas/index.js
@@ -0,0 +1,15 @@
+import Pizza from './Pizza'
+ 
+ export const schemaTypes = [
+    Pizza
+ ]
+
+// import createSchema from 'part:@sanity/base/schema-creator'
+// import schemaTypes from 'all:part:@sanity/base/schema-type'
+
+// export default createSchema({
+//     name: 'default',
+//     types: schemaTypes.concat([
+
+//     ]),
+// })
